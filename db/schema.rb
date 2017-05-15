e9e121ad7ae715d81796397654ca729eb922cf26
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-05-15 16:48:15 -0400

    Teachers have a first and last name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170515193852) do
+ActiveRecord::Schema.define(version: 20170515204646) do
 
   create_table "klasses", force: :cascade do |t|
     t.string   "name"
@@ -38,6 +38,8 @@ ActiveRecord::Schema.define(version: 20170515193852) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
     t.index ["email"], name: "index_teachers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_teachers_on_reset_password_token", unique: true
   end
--- spec/models/teacher_spec.rb
+++ spec/models/teacher_spec.rb
@@ -12,11 +12,15 @@ RSpec.describe Teacher, type: :model do
     it "has an email address and password" do
         teacher = Teacher.new(email:"test@example.com", password:"password")
         expect(teacher.email).to eq("test@example.com")
-        expect(teacher.password_digest).to_not eq(nil)
+        expect(teacher.password).to_not be_nil
     end
     
     it "is invalid without an email address and password" do
+        teacher = Teacher.create
+        expect(teacher).to_not be_valid
     end
     it "is valid with just email address and password" do
+        teacher = Teacher.create(email:"test@example.com", password:"password")
+        expect(teacher).to be_valid
     end
 end
