1713c8054e162600afe05dff0952f839f9e649ff
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-05-15 16:02:49 -0400

    Installed rspec tests

--- spec/models/teacher_spec.rb
+++ spec/models/teacher_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Teacher, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    before(:each) do
+        
+    end
+    it "has a first and last name" do
+        teacher = Teacher.new(first_name:"Suzie", last_name:"Smith")
+        expect(teacher.first_name).to eq("Suzie")
+        expect(teacher.last_name).to eq("Smith")
+    end
+    it "has an email address and password" do
+        teacher = Teacher.new(email:"test@example.com", password:"password")
+        expect(teacher.email).to eq("test@example.com")
+        expect(teacher.password_digest).to_not eq(nil)
+    end
+    
+    it "is invalid without an email address and password" do
+    end
+    it "is valid with just email address and password" do
+    end
 end
